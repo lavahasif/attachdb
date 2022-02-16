b1284f70a9c95ed573c3866995204282c10bd7a6
lavahasif <lavahasif@gmail.com>
2022-02-16 10:50:17 +0530

    Initial commit

---
+++ attachdb/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using attachdb.repository;
+
+namespace attachdb
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            var coTxt = path + @"\co.txt";
+            var excelXlsx = path + @"\excel.xlsx";
+            CreateInitialfile(coTxt, excelXlsx);
+
+
+            // string path = new DirectoryInfo(Environment.CurrentDirectory).Parent.Parent.FullName;
+
+
+            var readAllText = System.IO.File.ReadAllText(coTxt);
+            var readexcel = excelXlsx;
+            // var connectionString = new connectionString();
+            connectionString.Connections = readAllText;
+
+            var repository = new Repository();
+            var service = new Service();
+            var readExcel = repository.ReadExcel(readexcel, "xlsx");
+            var convertDataTable = service.ConvertDataTable<Ex>(readExcel);
+            convertDataTable.ForEach(x =>
+            {
+                service.AttachDatabase(x.databse, x.mdf_file.ToString(), x.log_file);
+                // Console.WriteLine(x.databse);
+            });
+
+
+            // for (var i = readexcel.Length - 1; i >= 0; i--)
+            // {
+            //     var dt = readExcel.Rows;
+            //     var name = dt[i][0].ToString();
+            //     Console.WriteLine(name);
+            //    
+            // }
+        }
+
+        private static void CreateInitialfile(string coTxt, string excelXlsx)
+        {
+            if (!File.Exists(coTxt))
+            {
+                var capacity = Properties.Resources.co;
+                File.WriteAllText(coTxt, capacity);
+            }
+
+            if (!File.Exists(excelXlsx))
+            {
+                var capacity = Properties.Resources.excel;
+                File.WriteAllBytes(excelXlsx, capacity);
+            }
+        }
+    }
+}
---
+++ attachdb/repository/Service.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.Office.Interop.Excel;
+using DataTable = System.Data.DataTable;
+
+namespace attachdb.repository
+{
+    public class Service
+    {
+        public void AttachDatabase(String name, String mdf, String ldf)
+        {
+            try
+            {
+                // var connectionString = new connectionString();
+                SqlConnection con = new SqlConnection(connectionString.Connections);
+                con.Open();
+
+                // SqlDataAdapter da = new SqlDataAdapter("select name from sys.databases", con);
+                // DataTable dt = new DataTable();
+                // da.Fill(dt);
+                // var list = new List<string>();
+                // for (var i = 0; i < dt.Rows.Count; i++)
+                // {
+                //     var o = dt.Rows[i][0].ToString();
+                //     list.Add(o);
+                // }
+
+
+                // if (!list.Contains(name))
+                // {
+                Console.WriteLine(name);
+                // setStored(name, mdf, con);
+                setStext(name, mdf, ldf, con);
+                // }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        private static void setStored(string name, string mdf, SqlConnection con)
+        {
+            SqlCommand cmd = new SqlCommand("sp_attach_db");
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@dbname", name);
+            cmd.Parameters.AddWithValue("@filename1", mdf);
+            cmd.ExecuteNonQuery();
+        }
+
+        private static void setStext(string name, string mdf, string ldf, SqlConnection con)
+        {
+            var sql =
+                $@" CREATE DATABASE {name}  ON   (   FILENAME = '{mdf}') LOG ON  (    FILENAME = '{ldf}') FOR ATTACH; ";
+            SqlCommand cmd = new SqlCommand(sql);
+            cmd.Connection = con;
+            cmd.CommandType = CommandType.Text;
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public List<Ex> ConvertDataTable<T>(DataTable dt)
+        {
+            List<Ex> data = new List<Ex>();
+            foreach (DataRow row in dt.Rows)
+            {
+                // T item = GetItem<T>(row);
+                data.Add(new Ex()
+                {
+                    databse = row.ItemArray[0].ToString(),
+                    mdf_file = row.ItemArray[1].ToString(),
+                    log_file = row.ItemArray[2].ToString(),
+                });
+            }
+
+            return data;
+        }
+    }
+}
+
+public class Ex
+{
+    public string databse { get; set; }
+    public string mdf_file { get; set; }
+    public string log_file { get; set; }
+}
+
+class connectionString
+{
+    public static String Connections { get; set; } = "";
+}
